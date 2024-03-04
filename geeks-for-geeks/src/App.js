79ca5f7c9a23ae40ec67aab8dc38ffea25b42a31
kapilramayanam <142371296+kapilramayanam@users.noreply.github.com>
2024-03-04 16:43:38 -0600

    no editor.js everything is in app.js
    deleted the editor.js

--- geeks-for-geeks/src/App.js
+++ geeks-for-geeks/src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import Editor from './Editor/Editor';
+import React, {useState} from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+//<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 
 function App() {
+  const [value, setValue] = useState('');
+    
+    const modules = {
+        toolbar : [
+            [{ 'header': [1, 2, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            ['link', 'image'],
+            [{'color' : []}],
+            [{'background' : []}]
+            
+
+        ],
+    }
+    const handleChange = (content) => {
+        setValue(content);
+    };
   return (
     <div className="App">
-        <Editor />
+       <div>
+            <ReactQuill  modules = {modules} theme="snow" value={value} onChange={handleChange}       
+/>
+        </div>
   
       <form id="pdfForm">
       <button type="submit" class="btn-submit">Generate PDF</button>
